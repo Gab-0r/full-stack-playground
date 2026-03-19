1f526af7ca5c249dc97788b2b945d56efa1e2bee
Gab-0r <j_gabrielorozco@live.com>
2026-03-19 05:54:13 -0500

    - Library app - Consuming first endpoint with React

--- projects/libraryapp/ui/library-app-ui/src/App.tsx
+++ projects/libraryapp/ui/library-app-ui/src/App.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import { useEffect, useState } from "react";
+import ListGroup from "./components/ListGroup";
+
+const base_url = `${import.meta.env.VITE_API_BASE_URL}`;
+
+interface Member {
+  name: string;
+  email: string;
+  membershipDate: string;
+  id: number;
+}
 
 export default function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const [items, setItems] = useState<Member[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `${base_url}/api/members`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setItems(data);
+    };
+    fetchData();
+  }, []);
+
+  const memberNames: string[] = items.map((member) => member.name);
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>name</Button>
+      <ListGroup items={memberNames} heading="Members"></ListGroup>
     </div>
   );
 }
--- projects/libraryapp/ui/library-app-ui/src/components/ListGroup.tsx
+++ projects/libraryapp/ui/library-app-ui/src/components/ListGroup.tsx
@@ -1,30 +1,24 @@
-import { useState } from "react";
 interface ListGroupProps {
   items: string[];
   heading: string;
-  onSelectedItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading, onSelectedItem }: ListGroupProps) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+function ListGroup({ items, heading }: ListGroupProps) {
+  if (items.length === 0) {
+    return (
+      <>
+        <h1>{heading}</h1>
+        <h2>No members signed</h2>
+      </>
+    );
+  }
 
   return (
     <>
       <h1>{heading}</h1>
       <ul className="list-group">
-        {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
-            key={item}
-            onClick={() => {
-              setSelectedIndex(index);
-              onSelectedItem(item);
-            }}
-          >
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
             {item}
           </li>
         ))}
